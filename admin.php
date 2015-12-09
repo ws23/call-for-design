d71930c1992e71e986fe9a31e847295c7a04cc07
Yu-Tzu Lin <linyutsu23@gmail.com>
2015-12-10 04:20:32 +0800

    first commit (template)

---
+++ admin.php
@@ -0,0 +1,14 @@
+<?php 
+	session_start(); 
+	require(dirname(__FILE__) . "/lib/header.php");
+?>
+
+<div class="container">
+		
+
+
+</div>
+
+<?php 
+	require(dirname(__FILE__) . "/lib/footer.php"); 
+?>
